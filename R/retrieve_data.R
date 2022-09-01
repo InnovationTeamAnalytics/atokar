7886309c027abaac3269d9b2149f65130db1aeec
Alejandro <a.abraham@innovationteam.eu>
2022-09-01 07:29:41 +0100

    AA - Functions setup

--- R/retrieve_data.R
+++ R/retrieve_data.R
@@ -17,7 +17,7 @@
 #'
 
 
-get_companies = function(id, pack = 'base', token = '5c64eb89575f4531853980280f4bfa03-e', lim = 1L) {
+get_companies_base = function(id, pack = 'base', token = '5c64eb89575f4531853980280f4bfa03-e', lim = 1L) {
 
   # 1. Check internet connection
   if (!curl::has_internet()) {
@@ -71,9 +71,16 @@ get_companies = function(id, pack = 'base', token = '5c64eb89575f4531853980280f4
 
     )
 
-  res = httr::GET(stringa)
 
-  if (res$status_code != 200) {
+  ### Response Decode
+
+  res = httr::GET(stringa)
+  dat = as.data.table(fromJSON(rawToChar(res$content))$items)
+
+
+  ### Output Preparation
+
+  if (dim(dat)[[1]] == 0) {
 
     warning(paste('[COMPANY ID]:', id, 'Not found in Atoka DB'), call. = FALSE)
 
@@ -104,26 +111,41 @@ get_companies = function(id, pack = 'base', token = '5c64eb89575f4531853980280f4
 
   dat = as.data.table(fromJSON(rawToChar(res$content))$items)
 
+    if(!is.null(dat$id)) {d_id = dat$id} else {d_id = '<Empty>'}
+    if(!is.null(dat$base.legalName)) {d_legalName = dat$base.legalName} else {d_legalName = '<Empty>'}
+    if(!is.null(dat$base.legalClass)) {d_legalClass = dat$base.legalClass} else {d_legalClass = '<Empty>'}
+    if(!is.null(dat$cervedId)) {d_cervedId = dat$cervedId} else {d_cervedId = '<Empty>'}
+    if(!is.null(dat$base.vat)) {d_vatId = dat$base.vat} else {d_vatId = '<Empty>'}
+    if(!is.null(dat$base.taxId)) {d_taxId = dat$base.taxId} else {d_taxId = '<Empty>'}
+
+    if(!is.null(dat$base.inGroup)) {d_inGroup = dat$base.inGroup} else {d_inGroup = '<Empty>'}
+    if(!is.null(dat$base.founded)) {d_dateFounded = dat$base.founded} else {d_dateFounded = '<Empty>'}
+
+    if(!is.null(dat$base.ateco[[1]]$code)) {d_atecoCode = dat$base.ateco[[1]]$code} else {d_atecoCode = '<Empty>'}
+    if(!is.null(dat$base.ateco[[1]]$rootCode)) {d_atecoRoot = dat$base.ateco[[1]]$rootCode} else {d_atecoRoot = '<Empty>'}
+
+    if(!is.null(dat$base.registeredAddress.provinceCode)) {d_areaProvince = dat$base.registeredAddress.provinceCode} else {d_areaProvince = '<Empty>'}
+    if(!is.null(dat$base.registeredAddress.macroregion)) {d_areaGeo = dat$base.registeredAddress.macroregion} else {d_areaGeo = '<Empty>'}
+
   data = data.table(
 
     KEY = id,
     SUCCESS = 'YES',
-    atokaId = dat$id,
-    legalName = dat$base.legalName,
-    legalClass = dat$base.legalClass,
-    cervedId = dat$cervedId,
-    vatId = dat$base.vat,
-    taxId = dat$base.taxId,
+    atokaId = d_id,
+    legalName = d_legalName,
+    legalClass = d_legalClass,
+    cervedId = d_cervedId,
+    vatId = d_vatId,
+    taxId = d_taxId,
 
-    inGroup = dat$base.inGroup,
-    dateFounded = dat$base.founded,
+    inGroup = d_inGroup,
+    dateFounded = d_dateFounded,
 
-    atecoCode = dat$base.ateco[[1]]$code,
-    atecoRoot = dat$base.ateco[[1]]$rootCode,
+    atecoCode = d_atecoCode,
+    atecoRoot = d_atecoRoot,
 
-    areaProvince = dat$base.registeredAddress.provinceCode,
-    areaGeo = dat$base.registeredAddress.macroregion
-
+    areaProvince = d_areaProvince,
+    areaGeo = d_areaGeo
 
   )
 
@@ -134,5 +156,148 @@ get_companies = function(id, pack = 'base', token = '5c64eb89575f4531853980280f4
 }
 
 
-PROVA = get_companies(id = 2)
-PROVA
+
+
+
+
+#' Scores Packages Data
+#'
+#' The main function to Retrieve from Atoka API data.
+#'
+#' @param id is the Company's Codice Fiscale, P.IVA, or Atoka ID
+#' @param pack is the Atoka API V2 Packages to access among base, economics, atokaIndicators, cervedIndicators and foreignMarket
+#' @param key the API TOKEN
+#' @param lim the number of companies to retrieve in case of multiple matches
+#'
+#' @import data.table
+#' @import httr
+#' @import jsonlite
+#'
+#' @return a data.table with the data available for the company
+#'
+#' @export
+#'
+
+
+get_companies_scores = function(id, pack = 'atokaIndicators', token = '5c64eb89575f4531853980280f4bfa03-e', lim = 1L) {
+
+  # 1. Check internet connection
+  if (!curl::has_internet()) {
+    warning("No internet connection. Please check your network. Could not download data series.", call. = FALSE)
+    return(invisible(NULL))
+  }
+
+  if(!is.character(id)) {
+
+    id = as.character(id)
+    warning("[ID] Not a Character String", call. = FALSE)
+
+  }
+
+  if(!is.character(pack)) {
+
+    pack = as.character(pack)
+    warning("[PACK] Not a Character String", call. = FALSE)
+
+  }
+
+  if(!is.character(key)) {
+
+    key = as.character(key)
+    warning("[KEY] Not a Character String", call. = FALSE)
+
+  }
+
+  if(!is.integer(lim)) {
+
+    lim = as.integer(lim)
+    warning("[LIM] Not an Integer or Natural Number", call. = FALSE)
+
+  }
+
+
+  ### Call Preparation
+
+  base_url = 'https://api.atoka.io/v2/'
+  endpoint = 'companies'
+
+  url = paste0(base_url, endpoint)
+
+    stringa = paste0(
+
+      url, '?',
+      'packages=', pack, '&',
+      'regNumbers=', id, '&',
+      'token=', token, '&',
+      'limit=', lim
+
+    )
+
+
+  ### Response Decode
+
+  res = httr::GET(stringa)
+  dat = as.data.table(fromJSON(rawToChar(res$content))$items)
+
+
+  ### Output Preparation
+
+  if (dim(dat)[[1]] == 0) {
+
+    warning(paste('[COMPANY ID]:', id, 'Not found in Atoka DB'), call. = FALSE)
+
+    data = data.table(
+
+      KEY = id,
+      SUCCESS = 'NO',
+      atokaId = '<Empty>',
+      legalName = '<Empty>',
+      cervedId = '<Empty>',
+
+      scr_innovation_value = '<Empty>',
+      scr_innovation_class = '<Empty>',
+
+      scr_centweb_value = '<Empty>',
+      scr_centweb_class = '<Empty>',
+
+
+    )
+
+  } else {
+
+  dat = as.data.table(fromJSON(rawToChar(res$content))$items)
+
+    if(!is.null(dat$id)) {d_id = dat$id} else {d_id = '<Empty>'}
+    if(!is.null(dat$base.legalName)) {d_legalName = dat$base.legalName} else {d_legalName = '<Empty>'}
+    if(!is.null(dat$cervedId)) {d_cervedId = dat$cervedId} else {d_cervedId = '<Empty>'}
+
+    if(!is.null(dat$atokaIndicators.innovation.score)) {d_scr_innovation_value = dat$atokaIndicators.innovation.score} else {d_scr_innovation_value = '<Empty>'}
+    if(!is.null(dat$atokaIndicators.innovation.scoreLabel)) {d_scr_innovation_class = dat$atokaIndicators.innovation.scoreLabel} else {d_scr_innovation_class = '<Empty>'}
+
+    if(!is.null(dat$atokaIndicators.webCentrality.score)) {d_scr_centweb_value = dat$atokaIndicators.webCentrality.score} else {d_scr_centweb_value = '<Empty>'}
+    if(!is.null(dat$atokaIndicators.webCentrality.scoreLabel)) {d_scr_centweb_class = dat$atokaIndicators.webCentrality.scoreLabel} else {d_scr_centweb_class = '<Empty>'}
+
+  data = data.table(
+
+      KEY = id,
+      SUCCESS = 'NO',
+      atokaId = d_atokaId,
+      legalName = d_legalName,
+      cervedId = d_cervedId,
+
+      scr_innovation_value = d_scr_innovation_value,
+      scr_innovation_class = d_scr_innovation_class,
+
+      scr_centweb_value = d_scr_centweb_value,
+      scr_centweb_class = d_scr_centweb_class,
+
+  )
+
+  }
+
+  return(data)
+
+}
+
+
+
--- inst/scratch.R
+++ inst/scratch.R
@@ -5,11 +5,11 @@ library(jsonlite)
 
 dt_companies = fread(file.path('inst', 'company_list.csv'))
 
-key = '5c64eb89575f4531853980280f4bfa03-e'
+token = '5c64eb89575f4531853980280f4bfa03-e'
 pack = 'base'
 lim = 1
 
-id = dt_companies[1,1][[1]]
+id = '00470550013'
 
 
 
@@ -22,13 +22,15 @@ url = paste0(base_url, endpoint)
 stringa = paste0(
 
   url, '?',
-  'packages=', pack, '&',
-  'regNumbers=', 2, '&',
+  'packages=', 'atokaI', '&',
+  'regNumbers=', id, '&',
   'token=', key, '&',
   'limit=', lim
 
 )
 
+called = httr::GET(stringa)
+called$status_code
 
 called_wrong = httr::GET(stringa)
 called_wrong$status_code
@@ -38,7 +40,14 @@ raw2char = rawToChar(called$content)
 toJSON = fromJSON(raw2char)
 dat = as.data.table(toJSON$items)
 
+dat = as.data.table(fromJSON(rawToChar(httr::content(called, encoding = "UTF-8")))$items)
 
 
-trial = get_companies(id = 2, pack = 'base', token = key)
 
+t_base_w = get_companies_base(id = '00470550013')
+t_base_w
+
+t_scores_w = get_companies_scores(id = '00470550013')
+t_scores_w
+
+t_scores_w = get_atoka_cvd(id = '01094190525')
